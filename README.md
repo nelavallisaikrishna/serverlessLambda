2136d091d096685e29577dcc38d5b75a7b9e9ce6
saikrishna <saikrishna@capitalquotient.com>
2019-03-01 14:39:17 +0530

    How to install and Config serverless

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # serverlessLambda
 How to create serverless
+
+
+This projected is created by using this Document
+
+https://hackernoon.com/a-crash-course-on-serverless-with-node-js-632b37d58b44
---
+++ handler.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports.hello = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.welcome = async(event, context) => {
+    return {
+      status: 200,
+        body: JSON.stringify({
+            message:'Wealcome to Lambda Function',
+            input : event
+        })
+    }
+}
